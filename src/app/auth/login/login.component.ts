f177fb00013ba36db2ecc3001d392b20ea4376d1
edwardlgrainger <edwardlgrainger@gmail.com>
2019-09-13 22:26:33 +0200

    login form with observable

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('',
+        { validators: [Validators.required, Validators.email],
+          asyncValidators: [this.forbiddenEmails] }),
+      password: new FormControl('',
+        { validators: [Validators.required] })
+    });
   }
 
+  onLogin() {
+    console.log(this.loginForm);
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      const forbiddenUsernames = ['aaa@bbb.com', 'fff@ggg.com'];
+      if (forbiddenUsernames.includes(control.value)) {
+        resolve({ emailIsForbidden: true });
+      } else {
+        resolve(null);
+      }
+    });
+    return promise;
+  }
 }
