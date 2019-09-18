d2b6222442cb46fcae85d2581bc7661d73e1c28a
edwardlgrainger <edwardlgrainger@gmail.com>
2019-09-18 23:37:56 +0200

    Adding a Spinner and Progress-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
+
+import { environment } from '../environments/environment';
+import { UIService } from './shared/ui.service';
+import { TrainingService } from './training/training.service';
+import { AuthService } from './auth/auth.service';
 
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
@@ -16,13 +22,9 @@ import { CurrentTrainingComponent } from './training/current-training/current-tr
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
-import { AuthService } from './auth/auth.service';
-import { TrainingService } from './training/training.service';
-import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -51,7 +53,7 @@ import { environment } from '../environments/environment';
     AngularFirestoreModule,
     AngularFireAuthModule
   ],
-  providers: [AuthService, TrainingService],
+  providers: [AuthService, TrainingService, UIService],
   bootstrap: [AppComponent],
   entryComponents: [StopTrainingComponent]
 })
