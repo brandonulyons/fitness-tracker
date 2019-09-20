a29845528ee111681781e61a55ad2ae95a8d8a9e
edwardlgrainger <edwardlgrainger@gmail.com>
2019-09-20 05:24:51 +0200

    Adding Auth Subscriptions

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -7,29 +7,24 @@ import {
   CanLoad,
   Route
 } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { take } from 'rxjs/operators';
 
-import { AuthService } from './auth.service';
+import * as fromRoot from '../app.reducer';
+
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanLoad {
   constructor(
-    private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private store: Store<{ui: fromRoot.State}>
   ) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.authService.isAuth()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-    }
+    return this.store.select(fromRoot.getIsAuth).pipe(take(1));
   }
 
   canLoad(route: Route) {
-    if (this.authService.isAuth()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-    }
+    return this.store.select(fromRoot.getIsAuth).pipe(take(1));
   }
 }
--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -2,29 +2,31 @@ import {
   Component,
   OnInit,
   EventEmitter,
-  Output,
-  OnDestroy
+  Output
 } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 import { AuthService } from '../../auth/auth.service';
+import * as fromRoot from '../../app.reducer';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss']
 })
-export class SidenavListComponent implements OnInit, OnDestroy {
+export class SidenavListComponent implements OnInit {
   @Output() closeSidenav = new EventEmitter<void>();
-  isAuth = false;
+  isAuth$: Observable<boolean>;
   authSubscription: Subscription;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private store: Store<{ui: fromRoot.State}>
+  ) {}
 
   ngOnInit() {
-    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
-      this.isAuth = authStatus;
-    });
+    this.isAuth$ = this.store.select(fromRoot.getIsAuth);
   }
 
   onClose() {
@@ -34,9 +36,5 @@ export class SidenavListComponent implements OnInit, OnDestroy {
   onLogout() {
     this.onClose();
     this.authService.logout();
-  }
-
-  ngOnDestroy() {
-    this.authSubscription.unsubscribe();
   }
 }
